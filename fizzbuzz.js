097f46ebba59ced10bb389294514475f1ce83ff7
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-20 15:55:20 +0000

    If xtple of 7, add 'Bang', xtple of 11, add 'Bong'. Changed logic

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,21 +6,23 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (let i = 1; i <= 20; i++)
+    for (let i = 1; i <= 105; i++)
     {
-        if (i % 3 ==0 && i % 5 == 0) {
-            console.log("FizzBuzz");
+        let output = "";
+        if (i % 3 == 0){
+            output += "Fizz";
         }
-        else if (i % 3 == 0){
-            console.log("Fizz");
+        if (i % 5 == 0) {
+            output += "Buzz";
         }
-        else if (i % 5 == 0) {
-            console.log("Buzz");
+        if (i % 7 == 0) {
+            output += "Bang";
         }
-        else {
-            console.log(i);
+        if (i % 11 == 0) {
+            output += "Bong"
         }
-    
+
+        console.log(i, output);
     }
 }
 
