a6163bcf728e362f39da1afc0a9fe51ed314a2ea
Stephanie Chiang <stephanie.chiang@softwire.com>
2025-01-21 11:28:00 +0000

    Added Bong + Fezz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,11 +6,18 @@ function fizzbuzz() {
     console.log('Welcome to FizzBuzz!');
 
     // Put your code here...
-    for (let i = 1; i <= 105; i++)
+    for (let i = 1; i <= 143; i++)
     {
         let output = "";
+
         if (i % 3 == 0){
             output += "Fizz";
+        }
+        if (i % 11 == 0) {
+            output = "Bong";
+        }
+        if (i % 13 == 0 || (i % 13 && i % 11)) { //Bong printed before fezz
+            output += "Fezz";
         }
         if (i % 5 == 0) {
             output += "Buzz";
@@ -18,10 +25,9 @@ function fizzbuzz() {
         if (i % 7 == 0) {
             output += "Bang";
         }
-        if (i % 11 == 0) {
-            output += "Bong"
-        }
-
+        // if (i % 17 == 0) {
+        //     let splitString = output.match(/[A-Z][a=z]+/g);
+        //     }
         console.log(i, output);
     }
 }
